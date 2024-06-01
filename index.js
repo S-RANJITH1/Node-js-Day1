476312ba42653ab6254435496f4f10ef57ff47d7
Ranjith Kumar <123442191+S-RANJITH1@users.noreply.github.com>
2024-06-01 18:42:42 +0530

    Changes

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express, { application } from "express";
+import fs from 'fs';
+import {format} from 'date-fns';
 //importing space
 
 
 //declaration/initialization
 const app = express();
-const PORT = 4000;
+const PORT = 5000;
 
 //Middleware
 app.use(express.json())
@@ -12,9 +14,33 @@ app.use(express.json())
 //routes
 app.get('/',(req,res)=>{
    //res.status(200).json({message:"Hi all welcome to our first node app"})
-res.status(200).send(`<div style="background-color:Aqua;color:black;font-size:100px">Welcome to our first app in node js</div>`)
-  })
+  let today = format(new Date(),'dd-mm-yyyy-HH-mm-ss')
+  //console.log(today);
+  const filepath = `Timestamp/${today}.txt`
+  fs.writeFileSync(filepath,`${today}`,'utf-8')
+  let data = fs.readFileSync(filepath,'utf-8')
+  try {
+  res.status(200).send(data)
+} catch (error) {
+  req.res(500).send('Internal Server Error')
+}
+})
 
+
+//New endpoint to retrieve all text files in a folder
+
+app.get("/getTextFiles",(req,res) => {
+  const folderpath = "Timestamp";
+  fs.readdir(folderpath, (err,files) => {
+  if (err) {
+    console.log(err);
+    res.status(500).send("An error occured while listing the files from directory");
+  } else {
+    const textFiles = files.filter((file) => path.extname(file) === ".txt");
+    res.status(200).json(textFiles);
+  }
+});
+});
 
 //running port
 app.listen(PORT,()=>{
